cc6ada12d1a2e9fb7c3eb141e5bfc683677328a5
Vladislav Minakov <125813927+asardinka@users.noreply.github.com>
2025-05-22 09:21:01 +0300

    Update Ogita_Aishima.h

--- algorithm_4/Ogita_Aishima.h
+++ algorithm_4/Ogita_Aishima.h
@@ -1,3 +1,14 @@
+// Название статьи:  Uchino Yuki, Terao Takeshi, Ozaki Katsuhisa. 2022.08.05 – Acceleration of
+// Iterative Refinement for Singular Value Decomposition.
+// (https://www.researchgate.net/publication/362642883_Acceleration_of_Iterative_Refinement_for_Singular_Value_Decomposition)
+// Обязательные условия: 
+// 1) В матрицах количествово строк больше или равному количеству столбцов
+// 2) каждое сингулярное значение меньше предыдущего (d1>d2>...>dn)
+//  каждое приближенное сингулярное значение отлично друг от друга (di != dj для i!=j)
+//
+// Выполнил Минаков В.С. КМБО-01-23
+// 
+
 #ifndef OGITA_AISHIMA_SVD_H
 #define OGITA_AISHIMA_SVD_H
 
@@ -52,39 +63,37 @@ protected:
         matrix Im = Matrix<long double, Dynamic, Dynamic>::Identity(m, m);
         matrix In = Matrix<long double, Dynamic, Dynamic>::Identity(n, n);
         
-        matrix R   = Im - Ud.transpose() * Ud;
-        matrix S  = In - Vd.transpose() * Vd;
+        matrix R = Im - Ud.transpose() * Ud;
+        matrix S_ = In - Vd.transpose() * Vd;
         matrix Tmn = Ud.transpose() * Ad * Vd;
 
         // Матрица сингулярных значений
         matrix Sigma_n = matrix::Zero(n, n);
         
         for (int i = 0; i < n; ++i) 
-            Sigma_n(i,i) = Tmn(i,i) / (1.0 - (( R(i,i) + S(i,i) ) * 0.5 ));  
+            Sigma_n(i,i) = Tmn(i,i) / (1.0 - (( R(i,i) + S_(i,i) ) * 0.5 ));  
 
         // Расширяем до m×n
         matrix Sigma = matrix::Zero(m, n);
         Sigma.block(0, 0, n, n) = Sigma_n;
 
-
-
-
         matrix R11 = R.topLeftCorner(n, n); // Верхний левый блок матрицы R размера n x n
         matrix T1  = Tmn.topRows(n); // Квадратная матрица n x n из первых n строк матрицы T
         matrix Calpha = T1 + R11 * Sigma_n;
-        matrix Cbeta = T1.transpose() + S * Sigma_n;
-        matrix D   = Sigma_n * Calpha + Cbeta * Sigma_n;
-        matrix E   = Calpha * Sigma_n + Sigma_n * Cbeta;
+        matrix Cbeta = T1.transpose() + S_ * Sigma_n;
+
+        matrix D = Sigma_n * Calpha + Cbeta * Sigma_n;
+        matrix E = Calpha * Sigma_n + Sigma_n * Cbeta;
 
         matrix G = matrix::Zero(n, n);
         matrix F = matrix::Zero(m, m);
 
         long double temp1, temp2;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; ++i)
         {
             temp1 = Sigma_n(i, i) * Sigma_n(i, i);
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n; ++j)
             {
                 if (i != j) {
                     temp2 = Sigma_n(j, j) * Sigma_n(j, j);
@@ -94,17 +103,17 @@ protected:
             }
         }
 
-        for (int i = 0; i < n; i++)
-            for (int j = n; j < m; j++)
+        for (int i = 0; i < n; ++i)
+            for (int j = n; j < m; ++j)
                  F(i,j) = -Tmn(j,i)/Sigma_n(i,i);
         
-        for (int i = n; i < m; i++)
-            for (int j = 0; j < n; j++)
+        for (int i = n; i < m; ++i)
+            for (int j = 0; j < n; ++j)
                 F(i,j) = R(i,j)-F(j,i);
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; ++i)
         {
-            G(i,i) = S(i,i) * 0.5;
+            G(i,i) = S_(i,i) * 0.5;
             F(i,i) = R(i,i) * 0.5;
         }
 
@@ -122,6 +131,7 @@ protected:
         ANS.Set_S(Sigma.template cast<T>());
         return ANS;
     }
+    
 
 public:
     Ogita_Aishima_SVD() {};
